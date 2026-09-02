718e0b394e43dbaa7a27ad200f9ccb393d5b8776
Ahire Nilima <104834048+NilimaAhire11@users.noreply.github.com>
2026-09-02 15:21:43 +0530

    Create Using Reverse Function.java

---
+++ String/Easy/Reverse String Using StringBuilder/Using Reverse Function.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main {
+
+    public static void main(String args[]){
+        StringBuilder sb = new StringBuilder("Hello");
+        sb.reverse();
+        System.out.println(sb);
+    }
+}
+
+//Time Complexity :- O(n)
+//Space Complexity :- O(1)
