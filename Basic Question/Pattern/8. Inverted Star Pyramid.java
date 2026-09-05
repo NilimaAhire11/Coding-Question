9f66b83e86dc6c018fd4a81aa4224a9673482f3e
Ahire Nilima <104834048+NilimaAhire11@users.noreply.github.com>
2026-09-05 15:41:08 +0530

    Create 8. Inverted Star Pyramid.java

---
+++ Basic Question/Pattern/8. Inverted Star Pyramid.java
@@ -0,0 +1,35 @@
+//Inverted Star Pyramid
+//Problem Statement: Given an integer N, print the following pattern :
+
+import java.util.*;
+public class Main {
+
+    public static void main(String args[]){
+      int n=5;
+      for(int i=n; i>=1; i--){
+          //space
+          for(int j=n-1; j>=i; j--){
+              System.out.print(" " + " ");
+          }
+          //star
+          for(int k=1; k<=2*i-1; k++){
+              System.out.print("*" +" ");
+          }
+          System.out.println();
+      }
+    }
+}
+
+
+OUTPUT
+
+* * * * * * * * * 
+  * * * * * * * 
+    * * * * * 
+      * * * 
+        * 
+
+
+//Complexity Analysis
+//Time Complexity: O(N²), since nested loops print approximately N² characters overall.
+//Space Complexity: O(1), as no extra data structures are required.
