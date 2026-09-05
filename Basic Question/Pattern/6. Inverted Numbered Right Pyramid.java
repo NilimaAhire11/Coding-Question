99c64e4ece2eafa63dbc52417c23cbb6c88b6c13
Ahire Nilima <104834048+NilimaAhire11@users.noreply.github.com>
2026-09-05 15:22:53 +0530

    Create 6. Inverted Numbered Right Pyramid.java

---
+++ Basic Question/Pattern/6. Inverted Numbered Right Pyramid.java
@@ -0,0 +1,32 @@
+//Inverted Numbered Right Pyramid
+//Problem Statement: Given an integer N, print the following pattern : 
+
+OUTPUT:- 
+1 2 3 4 5 
+1 2 3 4 
+1 2 3 
+1 2 
+1
+
+
+Code
+  
+import java.util.*;
+public class Main {
+
+    public static void main(String args[]){
+      int n=5;
+      for(int i=1; i<=n; i++){
+          for(int j=n; j>=i; j--){
+              System.out.print(n-j+1 + " ");
+          }
+          System.out.println();
+      }
+    }
+}
+
+
+
+//Complexity Analysis
+//Time Complexity: O(N²), because nested loops iterate across the triangular number of elements.
+//Space Complexity: O(1), as no extra data structures are used.
